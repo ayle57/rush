f98c1aca7a0814cd6ce0708dc9b04e7117ed4b21
ayle57 <allistair.jak@gmail.com>
2025-06-27 11:49:36 +0200

    Update

--- src/ajakopin/main.js
+++ src/ajakopin/main.js
@@ -1,3 +1,16 @@
 import "./components/navbar.js"
 import "./components/landing.js"
 import "./components/services.js"
+
+const linkLegal = document.querySelector(".link-legal");
+const linkUtil = document.querySelector(".link-util");
+
+linkLegal.addEventListener("click", (e) => {
+    e.preventDefault();
+    alert("C'est legal")
+})
+
+linkUtil.addEventListener("click", (e) => {
+    e.preventDefault();
+    alert("Pas besoin de condition d'utilisation, t'as juste a scroller pour utiliser le site")
+})
